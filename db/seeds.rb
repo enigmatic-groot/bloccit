efdad7db86d66fce85327fa83e329f6355a78b50
Jeremiah Judkins <jeremiah.a.judkins@gmail.com>
2018-10-30 09:15:18 -0700

    assignment-17-seeding-data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,22 @@ require 'random_data'
      body:   RandomData.random_paragraph
    )
  end
+
  posts = Post.all
+
+ puts "#{Post.count}"
+ Post.find_or_create_by(title: "Check out this unique title", body: "Check out this unique body")
+ puts "#{Post.count}"
+
+ puts "#{Comment.count}"
+ # This below comment uses a unique post I created as its post reference
+ Comment.find_or_create_by(post: posts.first, body:"By happenstance, this here is a body of uniqueness.")
+ puts "#{Comment.count}"
+
+
+
+ Comment.find_or_create_by()
+
 
  # Create Comments
  # #3
